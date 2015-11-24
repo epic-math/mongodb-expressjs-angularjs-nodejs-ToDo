44ac34dc051ddc2a0124fbd0c30716abd50e5ce0
bogotobogo <k@bogotobogo.com>
2015-11-24 00:29:33 -0800

    update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'ngTodo App' });
+  res.render('index', { title: 'Mongodb-Express-Angular-Node-ToDo' });
 });
 
 module.exports = router;
